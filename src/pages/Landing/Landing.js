246e3985c8273dd614da696f20049b37d8848a9f
Volodymyr Udovychenko <vosap2007@i.ua>
2021-06-06 23:49:51 +0300

    Стили для мобилки

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-//import pic from '../../images/desktop/landing.jpg';
-import  './Landing.css';
+import pic from '../../images/mobile/landingMobile.jpg';
+import LandingClasses from  './Landing.css';
 
 export default class Landing extends Component {
     render() {
         return (
-            <div>
-                <div className="box">
+            <div className="Page">
+                <img className='Page' src={pic}/>
+                <div className="Box">
                 <div className="logo">Questify</div>
                 <h1 className="title">Questify will turn your life into <br/>
                     a thrilling game full of amazing<br/>
                     quests and exciting challenges.</h1>
-                  <h2 className="appeal">Write your email to sign up or log in</h2>
-                  <input type="text" placeholder='Email' className="input"/>
-                  <input type="text" placeholder='Password' className="input"/>
+                  <h2 className="appeal">Choose your name to sign up or log in</h2>
+                  <input type="text" placeholder='Email' className="inputEmail"/>
+                  <input type="text" placeholder='Password' className="inputPassword"/>
                   <button className="button">go!</button>
                 </div>
             </div>
         )
     }
-    //<img className='pic1' src={pic}/>
 }
