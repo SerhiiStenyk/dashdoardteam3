212cebf6115ed76de322d0889a1eb6c6a2e86ea5
moonzpwr <60944420+moonzpwr@users.noreply.github.com>
2021-06-10 12:46:19 +0300

    логика распределения карточек
    добавил логику распределения карточек между today/tommorow/done
    +немного поправил глобальные стили для корректного отображения

--- src/components/Done/DoneBtn.js
+++ src/components/Done/DoneBtn.js
@@ -1,16 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 // import PropTypes from 'prop-types';
 import sprite from '../../sprite.svg';
 import s from './DoneBtn.module.css';
 import DoneList from './DoneList';
 import Container from '../Container/Container';
+import axios from 'axios';
 
 // import cards from './example.json';
+
+// надо изменить на глобальную переменную где будет лежать токен авторизации
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MGJkZmU4MzhiYjU0ZjZmMTVhYjU3ZjAiLCJzaWQiOiI2MGMwYWVmZDhiYjU0ZjZmMTVhYjU4ZmYiLCJpYXQiOjE2MjMyNDA0NDUsImV4cCI6MTYyMzI0NDA0NX0.lPt19jtkmB-CDRlnpi4MlWVMI1nBgJ9l5icSu9ATcQ8'
+const url = 'https://questify-backend.goit.global/card'
 
 export default function DoneBtn() {
     const [isOpen, setIsOpen] = useState(false);
     const toggling = () => setIsOpen(!isOpen);
 
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        axios.get(url, { headers: { "Authorization": `Bearer ${token}` } })
+            .then(({ data }) =>  setCards(data.cards))
+            .catch(err => console.log(err))
+    }, [])
 
     return (
         // <div className={s.container}>
@@ -25,9 +37,10 @@ export default function DoneBtn() {
             </button>
             <span className={s.dashed}></span>
             </div>
-            {isOpen && <DoneList cards={""}/>}
-
+            {isOpen && <DoneList cards={cards} />}
+            
             </Container>
+        
         
         // </div>
     );
--- src/components/Done/DoneList.js
+++ src/components/Done/DoneList.js
@@ -1,23 +1,31 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-// import Card from './Card/Card';
+// import Card from '../TodoCard/TodoCard';
 import s from './DoneList.module.css';
 
-const DoneList = ({cards}) => {
+const DoneList = ({ cards }) => {
+    const filtredCards = cards.filter(({ status }) =>  status === "Complete" ) 
+
     return (
         <ul className={s.list}>
-            <li>card1</li>
-            <li>card2</li>
-            {/* {cards.map(({level, title, date, time, group, id}) => (
-                <Card key={id}
-                    level={level}
-                    title={title}
-                    date={date}
-                    time={time}
-                    group={group}
-                />
+            {filtredCards.map(({difficulty, title, date, time, category}) => (
+                // <Card key={id}
+                //     level={level}
+                //     title={title}
+                //     date={date}
+                //     time={time}
+                //     group={group}
+                // />
+                <li>
+                    <ul>
+                        <li>{title }</li>
+                        <li>{ category}</li>
+                        <li>{difficulty }</li>
+                        <li>{date}, {time}</li>
+                    </ul>
+                </li>
             )
-            )} */}
+            )}
         </ul>
     );
 };
