e9343cccf8d2b053c0d1cddf1ce40431c032b6e8
Tetiana-Soliarova <t.s.skorpi1991@ukr.net>
2021-06-10 18:46:47 +0300

    modal
    модальне вікно на карточку. Логіка кнопки delete  ще не зроблена

---
+++ src/components/Modal/modal.js
@@ -0,0 +1,31 @@
+import style from './modal.module.css'
+
+const Modal = ({isOpened, onModalClose, title, children}) => {
+    
+    return (
+        <div className={isOpened ? style.active : style.modal}
+            >
+            <div className={style.modal_content}>
+
+                <div>
+                    <h2>{title}</h2>
+                        {children}
+                </div>
+                
+                <div className={style.modal_footer}>
+                <button
+                    className={style.modal__close}
+                    onClick={() => onModalClose(false)}
+                >
+                    Close
+                </button>
+                <div className={style.line
+                }></div>
+                    <button className={style.modal__delete} onClick={() => onModalClose(false)}>Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
