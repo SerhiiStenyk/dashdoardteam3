b3bebc340510d33f8be65a3de34921c0ba4f2c15
Volodymyr Udovychenko <vosap2007@i.ua>
2021-06-14 01:20:38 +0300

    Добавляет стандартный алерт

--- src/pages/Landing/LandingReg.js
+++ src/pages/Landing/LandingReg.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {authOperations} from '../../redux/auth';
 import {NavLink} from 'react-router-dom';
-import LandingClasses from  './Landing.scss';
+import './Landing.scss';
 
 class LandingReg extends Component {
     state = {
